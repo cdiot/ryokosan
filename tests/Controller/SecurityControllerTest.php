5bf16532a8b6aead25669c2d8bd6c770b15edec6
Chris <christopher.diot5@gmail.com>
2022-08-23 18:24:12 +0200

    Add SecurityControllerTest url smoke test

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testShouldDisplayLogin(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Connect to Ryokosan');
+    }
+
+    public function testVisitingWhileLoggedIn()
+    {
+        $client = static::createClient();
+
+        // get or create the user somehow (e.g. creating some users only
+        // for tests while loading the test fixtures)
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('foo@gmail.com');
+
+        $client->loginUser($testUser);
+
+        // user is now logged in, so you can test protected resources
+        $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Connect withother travelers');
+    }
+
+
+    public function testShouldDisplayLogout(): void
+    {
+        $client = static::createClient();
+
+        // get or create the user somehow (e.g. creating some users only
+        // for tests while loading the test fixtures)
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('foo@gmail.com');
+
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/logout');
+
+        $this->assertResponseRedirects();
+        $crawler = $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+}
