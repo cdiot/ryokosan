283e99ceafd9387b57ccfd28fb252cd987cc5fb7
Chris <christopher.diot5@gmail.com>
2022-08-25 22:45:41 +0200

    Add RegistrationControllerTest url smoke test

---
+++ tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public function testShouldDisplayRegister(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/registration');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Create your account on Ryokosan');
+    }
+
+    public function testShouldSubmitRegisterForm()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/registration');
+        $client->submitForm('Register', [
+            'registration_form[gender]' => 'f',
+            'registration_form[firstname]' => 'something',
+            'registration_form[email]' => 'foo123@gmail.com',
+            'registration_form[plainPassword]' => '123456',
+            'registration_form[birthday][month]' => 10,
+            'registration_form[birthday][day]' => 18,
+            'registration_form[birthday][year]' => 2005,
+            'registration_form[geolocalisation]' => 'GB',
+            'registration_form[sponsorship]' => 'something',
+            'registration_form[agreeTerms]' => true,
+        ]);
+        $this->assertResponseIsSuccessful();
+    }
+}
