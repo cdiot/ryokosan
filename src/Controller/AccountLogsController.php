ef7df751ef82ae543d2de9f149f9b8dc88fcaa5c
Chris <christopher.diot5@gmail.com>
2022-08-29 19:45:54 +0200

    Add AccountLogsControllerTest url smoke test

--- src/Controller/AccountLogsController.php
+++ src/Controller/AccountLogsController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\LogsRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class AccountLogsController extends AbstractController
 {
     #[Route(path: [
