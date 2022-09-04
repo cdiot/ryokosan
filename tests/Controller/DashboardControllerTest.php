b581606b6eec0cd3394238611c0cc9ec9c8c288c
Chris <christopher.diot5@gmail.com>
2022-09-04 16:38:58 +0200

    Add DashboardControllerTest url smoke test

---
+++ tests/Controller/DashboardControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DashboardControllerTest extends WebTestCase
+{
+    public function testShouldDisplayDashboard()
+    {
+        $client = static::createClient();
+        // get or create the user somehow (e.g. creating some users only
+        // for tests while loading the test fixtures)
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('bar@gmail.com');
+
+        $client->loginUser($testUser);
+
+        // user is now logged in, so you can test protected resources
+        $client->request('GET', '/admin');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Dashboard');
+    }
+}
