f074a29d542180111e1a3cd57928965c25994cb4
Chris <christopher.diot5@gmail.com>
2022-09-07 22:13:14 +0200

    Add KnpPaginator + pagination in activity index

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -6,6 +6,7 @@ use App\Entity\Activity;
 use App\Form\ActivityType;
 use App\Repository\ActivityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,11 +21,17 @@ class ActivityController extends AbstractController
         'fr' => '/activite',
         'it' => '/attività',
     ], name: 'app_activity_index', methods: ['GET'])]
-    public function index(ActivityRepository $activityRepository): Response
+    public function index(ActivityRepository $activityRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        return $this->render('activity/index.html.twig', [
-            'activities' => $activityRepository->findAll(),
-        ]);
+        $query = $activityRepository->findAll();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            6 /*limit per page*/
+        );
+
+        return $this->render('activity/index.html.twig', ['pagination' => $pagination]);
     }
 
     #[Route(path: [
