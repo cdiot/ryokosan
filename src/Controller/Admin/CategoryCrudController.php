002784cd160e13fc9880dc952ea71eb0b0fd175f
Chris <christopher.diot5@gmail.com>
2022-09-21 14:04:59 +0200

    Mapping Article + Category + Media entity in EasyAdmin

---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ColorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'label.name'),
+            SlugField::new('slug', 'label.slug')->setTargetFieldName('name'),
+            ColorField::new('color', 'label.color'),
+        ];
+    }
+}
